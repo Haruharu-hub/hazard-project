94b4a53edb3834ddbb0b595d61723ba1dc7f652b
Stephanie Ng <stephanienzx99@gmail.com>
2025-07-09 09:55:23 +1000

    Create custom_rules.py

---
+++ src/custom_rules.py
@@ -0,0 +1,110 @@
+rule_description = {
+
+"construction": 
+
+"""Rule 1: Ladder Use
+* Ladders shall be used only on stable and level surfaces unless secured to prevent accidental displacement. [1926.1053(b)(6)] 
+* The area around the top and bottom of ladders shall be kept clear. [1926.1053(b)(9)]
+* When ascending or descending a ladder, the user shall face the ladder. [1926.1053(b)(20)]
+* Each employee shall use at least one hand to grasp the ladder when progressing up and/or down the ladder. [1926.1053(b)(21)]
+* An employee shall not carry any object or load that could cause the employee to lose balance and fall. [1926.1053(b)(22)]
+
+Rule 2: Protective Equipment
+* Employees working in areas where there is a possible danger of head injury from impact, or from falling or flying objects, or from electrical shock and burns, shall be protected by protective helmets. [1926.100(a)]
+* Each affected employee uses appropriate eye or face protection when exposed to eye or face hazards from flying particles, molten metal, liquid chemicals, acids or caustic liquids, chemical gases or vapors, or potentially injurious light radiation. [1926.102(a)(1)]
+* Each employee on a walking/working surface (horizontal and vertical surface) with an unprotected side or edge which is 6 feet (1.8 m) or more above a lower level shall be protected from falling by the use of guardrail systems, safety net systems, or personal fall arrest systems. [1926.501(b)(1)]
+
+Rule 3: Fire Risk
+* Smoking shall be prohibited at or in the vicinity of operations which constitute a fire hazard, and shall be conspicuously posted: "No Smoking or Open Flame." [1926.151(a)(3)]
+* If the object to be welded, cut, or heated cannot be moved and if all the fire hazards cannot be removed, positive means shall be taken to confine the heat, sparks, and slag, and to protect the immovable fire hazards from them. [1926.352(b)]
+
+Rule 4: Crane Use
+* The operator must not engage in any practice or activity that diverts his/her attention while actually engaged in operating the equipment, such as the use of cellular phones (other than when used for signal communications). [1926.1417(d)]
+* Erect and maintain control lines, warning lines, railings or similar barriers to mark the boundaries of the hazard areas. [1926.1424(a)(2)(ii)]
+* While the operator is not moving a suspended load, no employee must be within the fall zone [1926.1425(b)]
+
+Rule 5: Scaffolding Risk
+* Each platform on all working levels of scaffolds shall be fully planked or decked between the front uprights and the guardrail supports [1926.451(b)(1)]
+* Guardrail systems shall be installed along all open sides and ends of platforms. [1926.451(g)(4)]
+* The top edge height of toprails or equivalent member on supported scaffolds shall be installed between 38 inches (0.97 m) and 45 inches (1.2 m) above the platform surface. [1926.451(g)(4)(ii)]
+* In addition to wearing hardhats each employee on a scaffold shall be provided with additional protection from falling hand tools, debris, and other small objects through the installation of toeboards, screens, or guardrail systems, or through the erection of debris nets, catch platforms, or canopy structures that contain or deflect the falling objects. [1926.451(h)(1)]
+""",
+
+"warehouse" : 
+
+"""Rule 1: Surface Condition
+* All places of employment, passageways, storerooms, service rooms, and walking-working surfaces are kept in a clean, orderly, and sanitary condition. [29 CFR 1910.22(a)(1)]
+* The floor of each workroom is maintained in a clean and, to the extent feasible, in a dry condition. When wet processes are used, drainage must be maintained and, to the extent feasible, dry standing places, such as false floors, platforms, and mats must be provided. [29 CFR 1910.22(a)(2)]
+* Walking-working surfaces are maintained free of hazards such as sharp or protruding objects, loose boards, corrosion, leaks, spills, snow, and ice. [29 CFR 1910.22(a)(3)]
+
+Rule 2: Ergonomic Lifting
+* Holding the load close to your body at waist height [General Duty Clause, Section 5(a)(1)]
+* Never lift a heavy item above shoulder level.
+* Never carry a load that obstructs your vision.
+
+Rule 3: Protective Equipment
+* Each affected employee uses appropriate eye or face protection when exposed to eye or face hazards from flying particles, molten metal, liquid chemicals, acids or caustic liquids, chemical gases or vapors, or potentially injurious light radiation [29 CFR 1910.133(a)(1)]
+* each affected employee wears a protective helmet when working in areas where there is a potential for injury to the head from falling objects. [29 CFR 1910.135(a)(1)]
+* Personal fall protection systems must be worn with the attachment point of the body harness located in the center of the employee's back near shoulder level. The attachment point may be located in the pre-sternal position if the free fall distance is limited to 2 feet (0.6 m) or less. [29 CFR 1910.140(c)(22)]
+
+Rule 4: Ladder Use
+* Ladders are used only on stable and level surfaces unless they are secured or stabilized to prevent accidental displacement; [29 CFR 1910.23(c)(4)]
+* Each employee faces the ladder when climbing up or down it; [29 CFR 1910.23(b)(11)]
+* Each employee uses at least one hand to grasp the ladder when climbing up and down it; and [29 CFR 1910.23(b)(12)]
+* No employee carries any object or load that could cause the employee to lose balance and fall while climbing up or down the ladder. [29 CFR 1910.23(b)(13)]
+
+Rule 5: Forklift Use
+* No person shall be allowed to stand or pass under the elevated portion of any truck, whether loaded or empty. [29 CFR 1910.178(m)(2)]
+* All traffic regulations shall be observed, including authorized plant speed limits. A safe distance shall be maintained approximately three truck lengths from the truck ahead, and the truck shall be kept under control at all times. [1910.178(n)(1)]
+* The driver shall be required to look in the direction of, and keep a clear view of the path of travel. [1910.178(n)(6)]
+""",
+
+"traffic": 
+
+"""Rule 1: Driving Distraction
+* A driver cannot operate a vehicle with an animal on their lap, or while the driver or a passenger is leading an animal, or while an animal is tied to a vehicle. [Road Safety Road Rules 2017 - reg 297] 
+* Drivers who hold a full licence in general cannot touch an unmounted portable device, such as a phone, tablet, laptop or any other device while driving. [New Vic Road Rules]
+* Anything that distracts the driver should be avoided. [New Vic Road Rules]
+* A driver must not operate a fatigue-regulated heavy vehicle on a road while impaired by fatigue. [Heavy Vehicle National Law]
+
+Rule 2: Traffic Rules
+* Drivers and riders must give way to anyone on or about to step onto a pedestrian crossing [Road Safety Road Rules 2017 Part 7]
+* Drivers and riders must not overtake another vehicle that has stopped at a pedestrian crossing. [Road Safety Road Rules 2017 Part 7]
+* If traffic lights at an intersection or marked foot crossing are showing a red traffic light, a driver must not enter the intersection or marked foot crossing. [Road Safety Road Rules 2017 - reg 59]
+* A driver on the part of the road that is a service road (except a service road to which a two-way sign applies) must drive in the same direction. [Road Safety Road Rules 2017 - reg 136]
+* A person must not cause a hazard to any person or vehicle by opening a door of a vehicle leaving a door of a vehicle open, or getting off, or out of, a vehicle. [Road Safety Road Rules 2017 - reg 269]
+
+Rule 3: Pedestrian Crossing
+* Pedestrians are not allowed to cross the road if they are facing an amber or red traffic or pedestrian light. [Road Safety Road Rules 2017 Part 14]
+* Pedestrians are not allowed to cause a traffic hazard by moving into the path of a driver. [Road Safety Road Rules 2017 Part 14]
+
+Rule 4: Road Condition
+* A person who drives a motor vehicle on a highway must drive in a safe manner having regard to all the relevant factors include the following— [ROAD SAFETY ACT 1986 - SECT 17A]
+    (a) the physical characteristics of the road;
+    (b) the prevailing weather conditions;
+    (c) the level of visibility;
+    (d) the condition of any vehicle the person is driving or riding on the highway;
+    (e) the prevailing traffic conditions;
+    (f) the relevant road laws and advisory signs;
+    (g) the physical and mental condition of the driver or road user.
+
+    Examples include:
+    - Depressions or potholes in the pavement that can cause vehicle damage or accidents.
+    - Debris or foreign objects that obstruct or endanger vehicles.
+    - Presence of animals on or near roads causing collision risks.
+    - Water accumulation causing hydroplaning or road damage.
+    - Construction zones, detours, or lane closures posing risks.
+    - Environmental conditions reducing visibility or traction.
+    - Tree branches or plants obstructing clear sight lines.
+
+Rule 5: Vehicle Load
+* The following requirements apply to a vehicle that is carrying a load: [ROAD SAFETY (VEHICLES) REGULATIONS 2021 - REG 285]
+    (a) the load must be secured by a means that is appropriate to the vehicle and the nature of the load;
+    (b) the load must be placed and secured on the vehicle in a way that prevents, or would be likely to prevent, the load or any part of the load from—
+        (i) hanging or projecting from the vehicle; or
+        (ii) becoming dislodged or falling from the vehicle;
+    (c) the load must not be placed or secured on the vehicle in a way that makes the vehicle unstable;
+    (d) the load must be placed and secured on the vehicle in compliance with the performance standards 
+""",
+
+}
